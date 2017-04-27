03a9530aa45c20d4c46eb2d5e6c2914fd5d8305d
Alex-Kinuthia <alexkinuthia125@gmail.com>
2017-04-27 15:19:54 +0300

    modified:   src/main/java/App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,13 @@
 //imports
 import java.util.*;
 
-//class app-main class
+//class app
 public class App {
+
+  // including random class and array list inorder for my game to select among them anonymously
+    public static void main(String[] args) {
+      List<Character> guess = new ArrayList<>();
+      Random randomNumber  = new Random();
+      String[] words =
+      { "testing", "boy", "girl", "boy", "sister", "docter","surgon", "branches", "dig", "crawl", "jump", "hurry", "slay", "sleepy", "noisy", "caring", "friend", "week", "day", "dark"
+      };
