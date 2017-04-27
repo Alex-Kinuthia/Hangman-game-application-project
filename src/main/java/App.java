6d452f6b46836dabb8f35ca76872edfb40531fb1
Alex-Kinuthia <alexkinuthia125@gmail.com>
2017-04-27 15:23:41 +0300

    modified:   src/main/java/App.java  new file:   src/main/java/Prompter.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -4,10 +4,15 @@ import java.util.*;
 //class app
 public class App {
 
-  // including random class and array list inorder for my game to select among them anonymously
+  // the words that would be detected
     public static void main(String[] args) {
       List<Character> guess = new ArrayList<>();
       Random randomNumber  = new Random();
       String[] words =
       { "testing", "boy", "girl", "boy", "sister", "docter","surgon", "branches", "dig", "crawl", "jump", "hurry", "slay", "sleepy", "noisy", "caring", "friend", "week", "day", "dark"
       };
+      Prompter prompter = new Prompter(new Hangman(words[randomNumber.nextInt(words.length)]));
+    	// Prompter prompter = new Prompter(new Hangman("testing"));
+    	  prompter.promptForGuess();
+        }
+    }
